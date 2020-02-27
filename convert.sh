401162f80d398d90096dde0b1d174402d1033c84
Ryan Nascimento <61243470+ryanguilherme@users.noreply.github.com>
2020-02-27 10:34:10 -0300

    Delete convert.sh

--- convert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-unzip imagens-livros.zip
-cd imagens-livros
-mkdir imagens-livros-png
-convert *.jpg *.png
-mv *png imagens-livros-png
-zip -r imagens-livros-png.zip /imagens-livros-png
