4b4a63a9bde0b6261c1e99104340bf8cfe4cb219
Ryan Nascimento <61243470+ryanguilherme@users.noreply.github.com>
2020-03-04 14:59:23 -0300

    Add files via upload

---
+++ script-for.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd imagens-livros
+
+for imagem in *.jpg
+do
+        echo $imagem
+        img_sem_ext=$(ls $imagem | awk -F. '{print $1}')
+        convert $imagem $img_sem_ext.png
+done
+
+cd ..
+
